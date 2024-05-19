bdde9f1dbe6b69e817f8020b37d90bab833000f1
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2024-05-19 12:29:52 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,20 @@
 import * as net from "net";
 
-const server = net.createServer((socket) => {
-	socket.end();
+const server = net.createServer((c) => {
+	// 'connection' listener.
+	console.log("client connected");
+
+	c.on("end", () => {
+		console.log("client disconnected");
+	});
+	c.write("HTTP/1.1 200 OK\r\n\r\n");
+	c.pipe(c);
 });
 
-server.listen(4221, "localhost", () => {
+server.on("error", (err) => {
+	throw err;
+});
+
+server.listen(4221, () => {
 	console.log("Server is running on port 4221");
 });
